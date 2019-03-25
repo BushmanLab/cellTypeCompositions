64d84d2f3c8210bb119b6eda762fad749e75d14b
Charles Berry <ccberry@ucsd.edu>
2019-03-25 16:45:20 -0700

    fix up probabilities before calling rmultinom()

--- src/ctcomps.cpp
+++ src/ctcomps.cpp
@@ -1,3 +1,4 @@
+
 #include <Rcpp.h>
 using namespace Rcpp;
 
@@ -10,9 +11,27 @@ using namespace Rcpp;
    Date: 13-03-2019
 */
 
+
+
+
+/* rmultnm assumes that:
+    - elts of prob sum to 1.0
+    - elts of rn have been initialized to zero
+   On exit rn should have a multinomial sample (or zeroes if n==0)
+*/
+
+/* do what FixupProb in main/random.c does */
+
+inline void rmultnm(int n, double* prob, int k, int* rn){
+    double prsum = 0.0;
+    for (int i=0; i<k; i++) prsum += prob[i];
+    for (int i=0; i<k; i++) prob[i] /= prsum;
+    Rf_rmultinom(n, prob,k,rn);
+}
+
 // [[Rcpp::export]]
 NumericMatrix samplePI(IntegerVector gi, NumericMatrix om, NumericVector pi0,
-		       int nkeep, int nthin = 1L, int nburn = 0L){
+		       int nkeep, int nthin = 1L, int nburn = 0L, double dprior = 1.0){
   int nsamps = (nkeep - 1L) * nthin + nburn + 1L;
   int isamp = 0L;
   int nrom = om.nrow();
@@ -32,7 +51,7 @@ NumericMatrix samplePI(IntegerVector gi, NumericMatrix om, NumericVector pi0,
     int R_minus_r = rnbinom(1L,r,rho_obs)[0L];
     NumericVector pi_miss = pi0 * (1.0 - rwom) / (1-rho_obs);
     IntegerVector dropped_f(nrom);
-    rmultinom(R_minus_r, REAL(pi_miss),nrom,INTEGER(dropped_f));
+    rmultnm(R_minus_r, REAL(pi_miss),nrom,INTEGER(dropped_f));
     NumericMatrix pi_given_gi(nrom,ncom); 
     for (int j = 0L; j<ncom; j++) {
       pi_given_gi( _, j ) = om( _, j ) * pi0;
@@ -42,12 +61,12 @@ NumericMatrix samplePI(IntegerVector gi, NumericMatrix om, NumericVector pi0,
     IntegerVector shuffled_f(nrom);
     for (int j=0L; j<ncom; j++){
       IntegerVector ftmp(nrom);
-      rmultinom( gi[j], REAL(pi_given_gi)+j*nrom, nrom, INTEGER(ftmp));
+      rmultnm( gi[j], REAL(pi_given_gi)+j*nrom, nrom, INTEGER(ftmp));
       shuffled_f = shuffled_f + ftmp;
     }
     NumericVector dirichlet_parm(nrom);
     for (int j = 0; j<nrom; j++){ 
-      dirichlet_parm[j] = 1.0 + (double) (shuffled_f[j] + dropped_f[j]);
+      dirichlet_parm[j] = dprior + (double) (shuffled_f[j] + dropped_f[j]);
     }
     NumericVector gamma_vals(nrom);
     for (int j = 0; j<nrom; j++) gamma_vals[j] = Rf_rgamma(dirichlet_parm[j],1.0);
