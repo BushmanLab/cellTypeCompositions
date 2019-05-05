03d3874469a56f8b5a5b763f5d2a69e18cdfff49
Charles Berry <ccberry@ucsd.edu>
2019-05-04 20:23:57 -0700

    add gibbsScan function

--- R/gibbsDPP.R
+++ R/gibbsDPP.R
@@ -19,7 +19,9 @@
 ##'     will result in r=termination with an error)
 ##' @return list with elements \code{eta}, \code{etaN], \code{etaM},
 ##'     and \code{dataToEta} which are updates to the correspondingly
-##'     named inputs.
+##'     named inputs for \code{gibbsDPP} and a list of two elements,
+##'     \code{last} and \code{launch}, given the last two such lists
+##'     for \code{gibbsCan}
 ##' @export
 ##' @author Charles Berry
 gibbsDPP <- function(
@@ -43,3 +45,90 @@ gibbsDPP <- function(
     auxGibbs(wtab, om, eta, etaN, dataToEta, etaM,
              auxM , alpha , verbose )
 }
+
+
+##' @rdname gibbsDPP
+##' @param nscans integer with value \code{nscans>=1L}. How many
+##'     scans to do.
+##' @param ctParms list of values for \code{nburn} and \code{ncore}
+##' @param ... unused at present
+##' @export
+gibbsScan <- function(wtab,
+		      om,
+		      eta=NULL,
+		      etaN=NULL,
+		      dataToEta=NULL, etaM=0L,
+		      alpha=1.0,auxM=10L,
+		      ctParms=NULL,
+		      nscans = 1L,
+		      etaCols=500L,
+		      verbose=FALSE,
+		      ...){
+  if (is.null(eta))  eta <- array(0.0,c(nrow(om),etaCols))
+   if (is.null(dataToEta)) dataToEta <- rep(-1L,length(wtab[["data.index"]]))
+  if (is.null(eta))
+    eta <- array(0.0,c(nrow(om), etaCols))
+  if (is.null(etaN)) etaN <- rep(0L, ncol(eta))
+  if (is.null(dataToEta)) dataToEta <- 
+			    rep(-1L,length(wtab$data.index))
+  stopifnot(length(etaN)==ncol(eta),
+	    nrow(om) == ncol(wtab[["tab"]]))
+  ctp <- list(nburn=10L, ncore=1L)
+  ctp[names(ctParms)] <- ctParms
+
+  pass1 <- auxGibbs(wtab, om, eta, etaN, dataToEta, etaM,
+		    auxM , alpha , verbose )
+
+  eta.by.ct <- table(pass1[["dataToEta"]],
+			wtab[["data.index"]]) %*% wtab[["tab"]]
+
+  eta.tuned <- sapply(ctSampler(eta.by.ct,om,
+				pass1[["eta"]][, 1:pass1[["etaM"]] ],
+				nkeep=1, # makes no sense otherwise
+				nburn=ctp[["nburn"]],
+				ncores=ctp[["ncore"]]),
+		      prop.table)
+
+
+  pass1[["eta"]][, 1:pass1[["etaM"]] ] <- eta.tuned
+
+  pass2  <- auxGibbs(wtab,om,
+		     pass1[["eta"]],
+		     pass1[["etaN"]],
+		     pass1[["dataToEta"]],
+		     etaM=pass1[["etaM"]],
+		     auxM=auxM,
+		     alpha=alpha,
+		     verbose=TRUE)
+
+
+  for (iscan in seq_len(nscans-1L)){
+    ## if there are updates, do them:
+    pass1 <- pass2
+
+    eta.by.ct <- table(pass1[["dataToEta"]],
+		       wtab[["data.index"]]) %*% wtab[["tab"]]
+
+    eta.tuned <- sapply(ctSampler(eta.by.ct,om,
+				  pass1[["eta"]][, 1:pass1[["etaM"]] ],
+				  nkeep=1, # makes no sense otherwise
+				  nburn=ctp[["nburn"]],
+				  ncores=ctp[["ncore"]]),
+			prop.table)
+
+    pass1[["eta"]][, 1:pass1[["etaM"]] ] <- eta.tuned
+
+    pass2  <- auxGibbs(wtab,om,
+		       pass1[["eta"]],
+		       pass1[["etaN"]],
+		       pass1[["dataToEta"]],
+		       etaM=pass1[["etaM"]],
+		       auxM=auxM,
+		       alpha=alpha,
+		       verbose=TRUE)
+  }
+
+  list(last=pass2, launch=pass1)
+
+}
+
