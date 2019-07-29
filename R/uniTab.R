fc305bfffc7ce23fdf47011814de97024a7d117e
Charles Berry <ccberry@ucsd.edu>
2019-07-29 09:59:07 -0700

    track rows omitted as zeros (for simulations)

--- R/uniTab.R
+++ R/uniTab.R
@@ -6,20 +6,29 @@
 ##' @title uniTab Combine Duplicate Rows
 ##' @param tab matrix or table of counts.  Rows correspond to lineages
 ##'     and columns to cell types.
-##' @param omitNullRows logical, if \code{TRUE} omit rows with only zero cells
+##' @param omitNullRows logical, if \code{TRUE} omit rows with only
+##'     zero cells
 ##' @return \code{list} with elements \code{tab} - the rows of
-##'     \code{unique(tab)}, \code{n} - a vector of replicate row counts, and
-##'     \code{data.index.index} - a mapping of the rows to the \code{tab}
-##'     argument to those of \code{uniTab(tab)[["tab"]]}.
+##'     \code{unique(tab)}, \code{n} - a vector of replicate row
+##'     counts, \code{data.index.index} - a mapping of the rows to the
+##'     \code{tab} argument to those of \code{uniTab(tab)[["tab"]]},
+##'     and \code{omitted} - \code{NULL} if no rows were omitted, or
+##'     an object such that \code{inverse.rle(result$omitted)} is a
+##'     logical vector indicating which table rows were all zeros.
 ##' @export
 ##' @author Charles Berry
 uniTab <- function(tab, omitNullRows=TRUE){
     tab <- unclass(as.matrix(tab))
     rownames(tab) <- NULL
-    if (omitNullRows && any(rz <- rowSums(tab)==0)) tab <- tab[!rz,]
+    if (omitNullRows && any(rz <- rowSums(tab)==0)) {
+        tab <- tab[!rz,]
+        omitted <- rle(rz)
+    } else {
+        omitted <- NULL
     utab <- unique(tab)
     tab.index <- match(
         do.call(paste,as.data.frame(tab)),
         do.call(paste,as.data.frame(utab)))
     list(tab=utab,n=as.vector(table(tab.index)),
-         data.index = tab.index)}
+         data.index = tab.index,
+         omitted = omitted)}
