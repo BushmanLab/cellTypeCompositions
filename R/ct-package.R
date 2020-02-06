49af4346aaaf919aaac10ffcfe1a0c86f2897036
Charles Berry <ccberry@ucsd.edu>
2020-02-06 13:47:20 -0800

    edit

--- R/ct-package.R
+++ R/ct-package.R
@@ -1,6 +1,6 @@
 #' @details
-#' The only function you're likely to need from cellTypeCompositions is 
-#' \code{ctSampler}
+#' The only functions you're likely to need from cellTypeCompositions is 
+#' \code{gibbsScan} and its \code{update} method
 #' @keywords package
 "_PACKAGE"
 #> [1] "_PACKAGE"
