6d73c6bee1bb0a01a6d0f2898ff99979a17282fc
Charles Berry <ccberry@ucsd.edu>
2020-02-06 17:55:39 -0800

    fix NAMESPACE

--- R/ct-package.R
+++ R/ct-package.R
@@ -1,6 +1,8 @@
 #' @details
 #' The only functions you're likely to need from cellTypeCompositions is 
 #' \code{gibbsScan} and its \code{update} method
+#' @import Rcpp
+#' @useDynLib cellTypeCompositions, .registration=TRUE
 #' @keywords package
 "_PACKAGE"
 #> [1] "_PACKAGE"
